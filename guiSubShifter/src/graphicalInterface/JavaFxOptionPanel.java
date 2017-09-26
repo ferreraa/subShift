20d0239c39ea7768f247d8aa07be0cb821260c12
Polytrope <alexandre78ferrera@gmail.com>
2017-09-25 23:07:14 -0500

    destination file field removed.
    From now on, the destination will be asked to the user once he validates
    the changes.
    The application also resists when the user tries to validate without
    filling one of the required fields. (there is a small problem with the
    popup window though. I suspect JavaFX to actually be the problem)
    
    
    Signed-off-by: Polytrope <alexandre78ferrera@gmail.com>

--- guiSubShifter/src/graphicalInterface/JavaFxOptionPanel.java
+++ guiSubShifter/src/graphicalInterface/JavaFxOptionPanel.java
@@ -15,6 +15,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -28,8 +29,9 @@ public class JavaFxOptionPanel extends Application implements EventHandler<Actio
 	private TextField srcField;
 
 	//browse destination
-	private Button browseDestButton;
+/*	private Button browseDestButton;
 	private TextField destField;
+*/
 
 	//time offset. This will be a TextField only accepting numbers.
 	private TextField numberField;
@@ -50,28 +52,29 @@ public class JavaFxOptionPanel extends Application implements EventHandler<Actio
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
-		grid.setVgap(10);
+		grid.setVgap(20);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 
-		Text scenetitle = new Text("Options");
-		scenetitle.setId("options");
+		Text scenetitle = new Text("Shift your subtitles!");
 		grid.add(scenetitle, 0, 0, 2, 1);
+//		scenetitle.setFont(Font.loadFont(JavaFxOptionPanel.class.getResource("CHICORY_.TFF").toExternalForm(),60));
+		scenetitle.setId("title");
 
 
 		//source file
 		Label originalSubs = new Label("subtitles to shift:");
-		grid.add(originalSubs, 0, 1);
+		grid.add(originalSubs, 0, 2);
 
 		srcField = new TextField();
-		grid.add(srcField, 1, 1);
+		grid.add(srcField, 1, 2);
 
 		browseSrcButton = new Button("Browse file...");
-		grid.add(browseSrcButton, 2, 1);
+		grid.add(browseSrcButton, 2, 2);
 		browseSrcButton.setOnAction(this);
 
 
 		//Destination
-		Label targetedFolder = new Label("Generated file path:");
+/*		Label targetedFolder = new Label("Generated file path:");
 		grid.add(targetedFolder, 0, 4);
 
 
@@ -81,11 +84,11 @@ public class JavaFxOptionPanel extends Application implements EventHandler<Actio
 
 		destField = new TextField();
 		grid.add(destField, 1, 4);
-
+*/
 
 		//Value of the requested time offset
-		Label timeOffset = new Label("define your time offset (ms)");
-		grid.add(timeOffset, 0, 6);
+		Label timeOffset = new Label("define your time offset (ms):");
+		grid.add(timeOffset, 0, 4);
 
 
 		//number Field
@@ -103,21 +106,21 @@ public class JavaFxOptionPanel extends Application implements EventHandler<Actio
 			}
 	    };
 
-		grid.add(numberField, 1, 6);
+		grid.add(numberField, 1, 4);
 
 		//faster or slower
 		CheckBox cb = new CheckBox("make subtitles faster");
 		cb.setIndeterminate(false);
-		grid.add(cb, 2, 6);
+		grid.add(cb, 2, 4);
 
 
 		//validate Button
 		validateButton = new Button("Validate");
-		grid.add(validateButton, 1, 8);
+		grid.add(validateButton, 1, 6);
 		validateButton.setOnAction(this);;
 
 
-		Scene scene = new Scene(grid, 350, 300);
+		Scene scene = new Scene(grid, 730, 350);
 		primaryStage.setScene(scene);
 		scene.getStylesheets().add
 		 (JavaFxOptionPanel.class.getResource("javaFxOptionPanel.css").toExternalForm());
@@ -129,48 +132,41 @@ public class JavaFxOptionPanel extends Application implements EventHandler<Actio
 	@Override
 	public void handle(ActionEvent e) {
 
-		if(e.getSource() == browseSrcButton || e.getSource() == browseDestButton) {
-			FileChooser fileChooser = new FileChooser();
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile;
+
+		if(e.getSource() == browseSrcButton) {
 			fileChooser.setTitle("Select subtitles File");
 			fileChooser.getExtensionFilters().add
 				(new ExtensionFilter("Subtitles Files", "*.srt"));
 
-			File selectedFile;
-			if (e.getSource() == browseSrcButton) {
-				selectedFile = fileChooser.showOpenDialog(primaryStage);
-				if (selectedFile != null)
-					srcField.setText(selectedFile.getAbsolutePath());
-			}
+			selectedFile = fileChooser.showOpenDialog(primaryStage);
+			if (selectedFile != null)
+				srcField.setText(selectedFile.getAbsolutePath());
+		}
+		else if (e.getSource() == validateButton) {
+
+			if(numberField.getText().equals("") || srcField.getText().equals(""))
+				Alerts.incompleteForm();
 			else {
 				selectedFile = fileChooser.showSaveDialog(primaryStage);
-				if (selectedFile != null)
-					destField.setText(selectedFile.getAbsolutePath());
-			}
+				if (selectedFile != null) {
+					int returnCode = Shifter.shift(srcField.getText(), Long.parseLong(numberField.getText()), selectedFile.getAbsolutePath());
+					System.out.println(returnCode);
 
-		} else if (e.getSource() == validateButton) {
-			int returnCode = Shifter.shift(srcField.getText(), Long.parseLong(numberField.getText()), destField.getText());
-			System.out.println(returnCode);
-
-			Alert alert;
-			if(returnCode == 0) {
-				alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Success");
-				alert.setHeaderText(null);
-				alert.setContentText("The subtitles have been modified with success!");
-
-				alert.showAndWait();
-			}
-			else if (returnCode == 255){
-				alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error");
-				alert.setHeaderText("Error 255");
-				alert.setContentText("One of the files couldn't be opened properly.");
-
-				alert.showAndWait();
+					if(returnCode == 0) {
+						Alerts.success();
+					}
+					else if (returnCode == 2){
+						Alerts.fileOpeningIssue();
+					}
+					else {
+						Alerts.unexpectedError(returnCode);
+					}
+				}
 			}
 		}
-
-
 	}
+
 
 }
