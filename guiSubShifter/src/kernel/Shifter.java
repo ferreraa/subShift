79350b9307fee2723a1801f14f5de9a7245e5d1e
Polytrope <alexandre78ferrera@gmail.com>
2017-09-27 12:11:43 -0500

    Now functional with an executable (.jar) under unix (java 8 required)
    To do so, the script is extracted from the jar, towards the /tmp
    directory. This is done only once, and only if necessary (so it works
    also if you launch the project from eclipse)

--- guiSubShifter/src/kernel/Shifter.java
+++ guiSubShifter/src/kernel/Shifter.java
@@ -1,55 +1,105 @@
 package kernel;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Shifter implements _Shifter {
 
-  private final String script = "ressources/subShifter2.0";  //path of the perlScript
+	private static String script = "ressources/subShifter2.0";  //path of the perlScript
 
-  private final String subFilePath;   //path (whith name) of the srt file
+	private final String subFilePath;   //path (whith name) of the srt file
 
-  private final String newPath;   //path (whith name) of the resulting file
+	private final String newPath;   //path (whith name) of the resulting file
 
-  private final long offset;   //offset required by the user.
+	private final long offset;   //offset required by the user.
 
-  public Shifter(String subFilePath, long offset, String newPath) {
-	  this.subFilePath = subFilePath;
-	  this.offset = offset;
-	  this.newPath = newPath;
-  }
+	private static boolean scriptUnzipped = false;	//indicates if the script was already unzipped or not.
 
-
-  public int shift() {
-    StringBuffer output = new StringBuffer();
-
-    String [] command = {script, subFilePath, ""+offset, newPath};
-
-    Process p;
-    try {
-      p = Runtime.getRuntime().exec(command);
-      p.waitFor();
-      BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-                        String line = "";
-      while ((line = reader.readLine())!= null) {
-        output.append(line + "\n");
-      }
+	public Shifter(String subFilePath, long offset, String newPath) {
+		this.subFilePath = subFilePath;
+		this.offset = offset;
+		this.newPath = newPath;
+	}
 
 
-      System.out.println(output.toString());
+	public int shift() {
 
-      return p.exitValue();
+		try {
+			return execCommand(true, script, subFilePath, ""+offset, newPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return 256;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return 259;
+		}
 
-    } catch (Exception e) {
-      e.printStackTrace();
-      return 255;
-    }
+	}
 
-  }
+	public static void unzipScript() throws IOException, InterruptedException {
 
-  public static int shift(String srcPath, long offset, String destPath) {
-	  return (new Shifter(srcPath, offset, destPath)).shift();
-  }
+		if(scriptUnzipped) {
+			System.err.println("unzipScript should be used only once. This is the second time so it won't have any effect.");
+			return;
+		} else if (new File(script).exists()) {
+			System.out.println("no need to try and unzip the script, we can already access the script from here: "+script);
+			scriptUnzipped = true;
+			return;
+		}
+
+		Path tempPath = Files.createTempDirectory("");
+
+		execCommand("unzip", "-d", tempPath.toString(), "subShifter.jar", script);
+		script = tempPath.toString() + "/" + script;
+
+		execCommand("chmod", "a+x", script);
+
+		scriptUnzipped = true;
+	}
+
+	public static int shift(String srcPath, long offset, String destPath) {
+		if(!scriptUnzipped)
+			try {
+				unzipScript();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return 257;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return 258;
+			}
+
+		return (new Shifter(srcPath, offset, destPath)).shift();
+	}
+
+
+	public static int execCommand(boolean printOutput, String...command) throws IOException, InterruptedException {
+		Process p = new ProcessBuilder(command).start();
+		BufferedReader reader =
+				new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+		int exitValue = p.waitFor();
+
+		String line = "";
+
+		StringBuffer output = new StringBuffer();
+
+		while ((line = reader.readLine())!= null) {
+			output.append(line + "\n");
+		}
+
+		if(printOutput) {
+			System.out.println(output.toString());
+		}
+
+		return exitValue;
+	}
+
+	public static int execCommand(String...command) throws IOException, InterruptedException {
+		return execCommand(false,command);
+	}
 }
